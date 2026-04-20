20cfa39eabaaa0ee8a03282fdf0839108bddf076
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2026-04-20 14:31:34 +0200

    Original lint attribute port attempt

--- compiler/rustc_lint/src/builtin.rs
+++ compiler/rustc_lint/src/builtin.rs
@@ -1903,9 +1903,10 @@ impl KeywordIdents {
             return;
         }
 
-        cx.emit_span_lint(
+        cx.sess().psess.buffer_lint(
             lint,
             ident.span,
+            CRATE_NODE_ID,
             BuiltinKeywordIdents { kw: ident, next: edition, suggestion: ident.span, prefix },
         );
     }
